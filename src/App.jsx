d07405d187270b17b52aadfe6216abb3931016eb
wilson7611 <106674260+wilson7611@users.noreply.github.com>
2024-02-08 16:36:24 -0400

    Se creo la lista de criptomonedas, se agrego axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-
-
+import { useEffect, useState } from "react"
+import axios from "axios"
 function App() {
+
+  
+  const [criptos, setCriptos] = useState()
+
+  useEffect(()=>{
+    axios.get(`https://api.coincap.io/v2/assets`)
+    .then((data) => {
+      setCriptos(data.data.data)
+    })
+    .catch(()=>{
+      console.error("la peticion fallo");
+    })
+  }, [])
+  //solucion a coneccion lenta
+  if(!criptos) return <><span>Cargando...</span></>
   return (
     <>
-      <h1>Hola mundo</h1>
-     
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        { criptos.map(({id,name, priceUsd}) => (
+          <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+        )) }
+      </ol>
     </>
-  )
+  );
 }
 
 export default App
